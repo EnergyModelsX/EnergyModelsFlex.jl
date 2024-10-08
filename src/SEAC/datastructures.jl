2ca5f056969ab0fe5170d1944138e7c58ed55ea0
Sigrid Aunsmo <sigrid.aunsmo@sintef.no>
2024-10-08 14:51:46 +0200

    nodes from SEAC

---
+++ src/SEAC/datastructures.jl
@@ -0,0 +1,76 @@
+using JuMP
+using HiGHS
+using YAML
+using TimeStruct
+using EnergyModelsBase
+using EnergyModelsRenewableProducers
+using Revise
+# using SEAC
+
+using Dates
+# using TimeZones
+using Plots
+# using MathOptInterface
+
+const TS = TimeStruct
+const EMB = EnergyModelsBase
+
+
+"""
+Unit commitment nodes
+"""
+abstract type UnitCommitmentNode{} <: EMB.NetworkNode end
+struct MinUpDownTimeNode{} <: UnitCommitmentNode
+    id
+    cap::TimeProfile
+    opex_var::TimeProfile
+    opex_fixed::TimeProfile
+    input::Dict{Resource, Real}
+    output::Dict{Resource, Real}
+    minUpTime::Real #number of operational periodes
+    minDownTime::Real  #number of operational periodes
+    minCapacity::Real
+    maxCapacity::Real
+    data::Array{Data}
+end
+struct ActivationCostNode{} <: UnitCommitmentNode
+    id
+    cap::TimeProfile
+    opex_var::TimeProfile
+    opex_fixed::TimeProfile
+    input::Dict{Resource, Real}
+    output::Dict{Resource, Real}
+    activation_time::Real
+    activation_consumption::Dict{Resource, Real}
+    data::Array{Data}
+end
+
+"""
+Electric battery node
+- c_rate: 
+- coloumbic_eff: 
+"""
+struct ElectricBattery{T} <: EMB.Storage{T}
+    id
+    charge::EMB.AbstractStorageParameters
+    level::EMB.UnionCapacity
+    c_rate::Real # 
+    coloumbic_eff::Real  # efficency, typically 0.98 
+    stor_res::Resource
+    input::Dict{<:Resource, <:Real}
+    output::Dict{<:Resource, <:Real}
+    data::Vector{<:Data}  
+end
+
+struct LoadShiftingNode <: EMB.Sink
+    id
+    cap::TimeProfile
+    penalty::Dict{Symbol, <:TimeProfile}
+    input::Dict{<:Resource, <:Real}
+    loadshifttimes::Vector{<:Real}
+    load_shifts_per_periode::Real
+    load_shift_duration::Real # in number
+    load_shift_magnitude::Real
+    n_loadshift::Real
+    data::Vector{Data} 
+end

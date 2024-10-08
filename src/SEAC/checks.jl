91dc349aff956c79179544a6ccdd6d4a4222e388
Sigmund Eggen Holm <sigmund.holm@sintef.no>
2024-10-08 15:28:33 +0200

    Add check.

---
+++ src/SEAC/checks.jl
@@ -0,0 +1,5 @@
+
+function EMB.check_node(n::MinUpDownTimeNode, 𝒯, modeltype::EnergyModel, check_timeprofiels::Bool)
+    # We need the minimum capacity to be greater than zero.
+    @assert n.minCapacity > 0
+end

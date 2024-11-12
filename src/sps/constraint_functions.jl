2d3002c825f11950bfd006be58161777a9603779
Sigmund Eggen Holm <sigmund.holm@sintef.no>
2024-11-12 11:11:32 +0100

    add sps things.

--- src/sps/constraint_functions.jl
+++ src/sps/constraint_functions.jl
@@ -1,4 +1,48 @@
 """
+    EMB.constraints_flow_in(m, n::StorageEfficiency, 𝒯::TimeStructure, ::EnergyModel)
+
+Function for creating the constraint on the inlet flow to a generic `StorageEfficiency`.
+This function serves as fallback option if no other function is specified for a `StorageEfficiency`.
+"""
+function EMB.constraints_flow_in(m, n::StorageEfficiency, 𝒯::TimeStructure, ::EnergyModel)
+    # Declaration of the required subsets
+    p_stor = storage_resource(n)
+    𝒫ᵃᵈᵈ = setdiff(inputs(n), [p_stor])
+
+    # Constraint for additional required input
+    @constraint(
+        m,
+        [t ∈ 𝒯, p ∈ 𝒫ᵃᵈᵈ],
+        m[:flow_in][n, t, p] == m[:flow_in][n, t, p_stor] * inputs(n, p)
+    )
+
+    # Constraint for StorageEfficiency rate usage for charging and discharging
+    @constraint(
+        m,
+        [t ∈ 𝒯],
+        m[:stor_charge_use][n, t] == m[:flow_in][n, t, p_stor] * inputs(n, p_stor)
+    )
+end
+
+"""
+    EMB.constraints_flow_out(m, n::StorageEfficiency, 𝒯::TimeStructure, ::EnergyModel)
+
+Function for creating the constraint on the outlet flow from a generic `StorageEfficiency`.
+This function serves as fallback option if no other function is specified for a `StorageEfficiency`.
+"""
+function EMB.constraints_flow_out(m, n::StorageEfficiency, 𝒯::TimeStructure, ::EnergyModel)
+    # Declaration of the required subsets
+    p_stor = storage_resource(n)
+
+    # Constraint for the individual output stream connections
+    @constraint(
+        m,
+        [t ∈ 𝒯],
+        m[:stor_discharge_use][n, t] * outputs(n, p_stor) == m[:flow_out][n, t, p_stor]
+    )
+end
+
+"""
     EMB.constraints_capacity(m, n::PayAsProducedPPA, 𝒯::TimeStructure, modeltype::EnergyModel)
 
 Function for creating the constraint on the maximum capacity of a `PayAsProducedPPA`.
@@ -38,4 +82,26 @@ function EMB.constraints_opex_var(m, n::PayAsProducedPPA, 𝒯ᴵⁿᵛ, ::Energ
             EMB.scale_op_sp(t_inv, t) for t ∈ t_inv
         )
     )
+end
+
+"""
+    constraints_flow_in(m, n::Combustion, 𝒯::TimeStructure, ::EnergyModel)
+
+Function for creating the constraint on the inlet flow to a generic `Node`.
+This function serves as fallback option if no other function is specified for a `Node`.
+"""
+function EMB.constraints_flow_in(m, n::Combustion, 𝒯::TimeStructure, ::EnergyModel)
+    # Declaration of the required subsets
+    𝒫ⁱⁿ = inputs(n)
+
+    # Constraint for the input stream connections
+    @constraint(
+        m,
+        [t ∈ 𝒯],
+        sum(m[:flow_in][n, t, p] * inputs(n, p) for p ∈ 𝒫ⁱⁿ) == m[:cap_use][n, t]
+    )
+
+    # Limit the fraction of an input resource relative to the total output
+    tot_flow_in = @expression(m, [t ∈ 𝒯], sum(m[:flow_in][n, t, p] for p ∈ 𝒫ⁱⁿ))
+    @constraint(m, [t ∈ 𝒯, p ∈ 𝒫ⁱⁿ], m[:flow_in][n, t, p] ≤ tot_flow_in[t] * limits(n, p))
 end
--- src/sps/datastructures.jl
+++ src/sps/datastructures.jl
@@ -1,7 +1,56 @@
+# Written by Jon Vegard in the case study SPS.
+
+"""
+    StorageEfficiency{T} <: EMB.Storage{T}
+
+A StorageEfficiency node which enables storage efficiency control compared to RefStorage{T}.
+
+It is designed as a parametric type through the type parameter `T` to differentiate between
+different cyclic behaviours. Note that the parameter `T` is only used for dispatching, but
+does not carry any other information. Hence, it is simple to fast switch between different
+[`StorageBehavior`](@ref)s.
+
+The current implemented cyclic behaviours are [`CyclicRepresentative`](@ref),
+[`CyclicStrategic`](@ref), and [`AccumulatingEmissions`](@ref).
+
+# Fields
+- **`id`** is the name/identifier of the node.
+- **`charge::AbstractStorageParameters`** are the charging parameters of the `Storage` node.
+  Depending on the chosen type, the charge parameters can include variable OPEX, fixed OPEX,
+  and/or a capacity.
+- **`level::AbstractStorageParameters`** are the level parameters of the `Storage` node.
+  Depending on the chosen type, the charge parameters can include variable OPEX and/or fixed OPEX.
+- **`stor_res::Resource`** is the stored [`Resource`](@ref).
+- **`input::Dict{<:Resource, <:Real}`** are the input [`Resource`](@ref)s with conversion
+  value `Real`.
+- **`output::Dict{<:Resource, <:Real}`** are the generated [`Resource`](@ref)s with conversion
+  value `Real`. Only relevant for linking and the stored [`Resource`](@ref) as the output
+  value is not utilized in the calculations.
+- **`data::Vector{<:Data}`** is the additional data (e.g. for investments). The field `data`
+  is conditional through usage of a constructor.
+"""
+struct StorageEfficiency{T} <: EMB.Storage{T}
+    id::Any
+    charge::EMB.UnionCapacity
+    level::EMB.UnionCapacity
+    stor_res::Resource
+    input::Dict{<:Resource,<:Real}
+    output::Dict{<:Resource,<:Real}
+    data::Vector{<:Data}
+end
+function StorageEfficiency{T}(
+    id,
+    charge::EMB.UnionCapacity,
+    level::EMB.UnionCapacity,
+    stor_res::Resource,
+    input::Dict{<:Resource,<:Real},
+    output::Dict{<:Resource,<:Real},
+) where {T<:EMB.StorageBehavior}
+    return StorageEfficiency{T}(id, charge, level, stor_res, input, output, Data[])
+end
+
 """
     PayAsProducedPPA <: EMB.Source
-
-Written by Jon Vegard in the case study SPS.
 
 A pay-as-produced ppa energy source. It extends the existing `PayAsProducedPPA` node through
 including a constraint on the opex_var such that curtailed energy is also included in the opex.
@@ -46,3 +95,49 @@ operational period `t`.
 """
 profile(n::PayAsProducedPPA) = n.profile
 profile(n::PayAsProducedPPA, t) = n.profile[t]
+
+"""
+    Combustion <: NetworkNode
+
+A `Combustion` node.
+The `Combustion` utilizes a linear, time independent conversion rate of the `input`
+[`Resource`](@ref)s to the output [`Resource`](@ref)s, subject to the available capacity.
+The capacity is hereby normalized to a conversion value of 1 in the fields `input` and
+`output`.
+
+# Fields
+- **`id`** is the name/identifier of the node.
+- **`cap::TimeProfile`** is the installed capacity.
+- **`opex_var::TimeProfile`** is the variable operating expense per energy unit produced.
+- **`opex_fixed::TimeProfile`** is the fixed operating expense.
+- **`limit::Dict{<:Resource, <:Real}`** are the limits for each [`Resource`](@ref)s of the total input.
+- **`input::Dict{<:Resource, <:Real}`** are the input [`Resource`](@ref)s with conversion
+  value `Real`.
+- **`output::Dict{<:Resource, <:Real}`** are the generated [`Resource`](@ref)s with
+  conversion value `Real`.
+- **`data::Vector{Data}`** is the additional data (e.g. for investments). The field `data`
+  is conditional through usage of a constructor.
+"""
+struct Combustion <: NetworkNode
+    id::Any
+    cap::TimeProfile
+    opex_var::TimeProfile
+    opex_fixed::TimeProfile
+    limit::Dict{<:Resource,<:Real}
+    input::Dict{<:Resource,<:Real}
+    output::Dict{<:Resource,<:Real}
+    data::Vector{<:Data}
+end
+function Combustion(
+    id,
+    cap::TimeProfile,
+    opex_var::TimeProfile,
+    opex_fixed::TimeProfile,
+    limit::Dict{<:Resource,<:Real},
+    input::Dict{<:Resource,<:Real},
+    output::Dict{<:Resource,<:Real},
+)
+    return Combustion(id, cap, opex_var, opex_fixed, limit, input, output, Data[])
+end
+limits(n::Combustion, p::Resource) = n.limit[p]
+limits(n::Combustion) = collect(keys(n.limit))

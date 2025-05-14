49d4f08fee15f836c0faac26b7b6c39c45e0bee5
Julian Straus <julian.straus@sintef.no>
2025-05-14 16:09:18 +0200

    Avoid creating variables based on the first instance

--- src/sink/model.jl
+++ src/sink/model.jl
@@ -1,26 +1,20 @@
 """
     EMB.variables_node(m, 𝒩ˢⁱⁿᵏ::Vector{<:PeriodDemandSink}, 𝒯, ::EnergyModel)
 
-Declare variables for the optimization model `m` related to period demand sink
-nodes `𝒩ˢⁱⁿᵏ` over the time structure `𝒯`. These variables represent the
-surplus and deficit in demand for each period.
+Creates the following additional variables for **ALL** [`PeriodDemandSink`](@ref) nodes:
+- `demand_sink_surplus[n, i]` is a non-negative variable indicating a surplus in demand in
+  each period `i`.
+- `demand_sink_deficit[n, i]` is a non-negative variable indicating a deficit in demand for
+  each period `i`.
 
-# Arguments
-- `m`: The optimization model.
-- `𝒩ˢⁱⁿᵏ`: A vector of period demand sink nodes.
-- `𝒯`: The time structure.
-- `modeltype`: The type of energy model.
-
-# Variables
-- `demand_sink_surplus[n, i]`: Non-negative variable indicating surplus in demand for each period `i`.
-- `demand_sink_deficit[n, i]`: Non-negative variable indicating deficit in demand for each period `i`.
-
+!!! note "Definition of period"
+    The period in the description above does not correspond to an operational period as known
+    from `TimeStruct`. Instead, it is a period in which the demand must be satisfied. A period
+    can consist of multiple operational periods.
 """
 function EMB.variables_node(m, 𝒩ˢⁱⁿᵏ::Vector{<:PeriodDemandSink}, 𝒯, ::EnergyModel)
-    n = first(𝒩ˢⁱⁿᵏ)
-    num_periods = number_of_periods(n, 𝒯)
-    @variable(m, demand_sink_surplus[𝒩ˢⁱⁿᵏ, i=1:num_periods] >= 0)
-    @variable(m, demand_sink_deficit[𝒩ˢⁱⁿᵏ, i=1:num_periods] >= 0)
+    @variable(m, demand_sink_surplus[n ∈ 𝒩ˢⁱⁿᵏ, i=1:number_of_periods(n, 𝒯)] >= 0)
+    @variable(m, demand_sink_deficit[n ∈ 𝒩ˢⁱⁿᵏ, i=1:number_of_periods(n, 𝒯)] >= 0)
 end
 
 """
@@ -65,18 +59,21 @@ end
 """
     EMB.variables_node(m, 𝒩ᴸˢ::Vector{<:LoadShiftingNode}, 𝒯, ::EnergyModel)
 
-Create the optimization variables for every time slots indicated by `load_shift_times`
- - `:load_shift_from[n, t]`, integer variable for how many batches shifted away from the this time slot
- - `:load_shift_to[n, t]`, integer variable for how many batches shifted to this time slot
-for every timestep in:
- - `:load_shifted[n ,t]`, continous variable for the total capacity load shifted from the time step
+Creates the following additional variables for **ALL** [`LoadShiftingNode`](@ref) nodes.
+- `load_shift_from[n, t]` is an integer variable for how many batches are shifted away from
+  time period `t`.
+- `load_shift_to[n, t]` is an integer variable for how many batches are shifted to the time
+  period `t`.
+- `:load_shifted[n ,t]` is a continous variable for the total capacity load shifted in
+  time period `t`. The variable can also be negative indicating a load shifted from this
+  time period.
+
+The individual time periods which allow for load shifting are declared by the parameter
+`load_shift_times`.
 """
 function EMB.variables_node(m, 𝒩ᴸˢ::Vector{<:LoadShiftingNode}, 𝒯, ::EnergyModel)
-    times = collect(𝒯)
-    𝒯ᴸˢ = times[𝒩ᴸˢ[1].load_shift_times]
-    # Creating a variable for every time step where load shifting is allowed
-    @variable(m, load_shift_from[𝒩ᴸˢ, 𝒯ᴸˢ] >= 0, Int)
-    @variable(m, load_shift_to[𝒩ᴸˢ, 𝒯ᴸˢ] >= 0, Int)
-    # Creating a variable for every timestep saying how much load is shifted
-    @variable(m, load_shifted[𝒩ᴸˢ, 𝒯]) # can also be negative which will mean load shifted from
+    ops = collect(𝒯)
+    @variable(m, load_shift_from[n ∈ 𝒩ᴸˢ, ops[n.load_shift_times]] >= 0, Int)
+    @variable(m, load_shift_to[n ∈ 𝒩ᴸˢ, ops[n.load_shift_times]] >= 0, Int)
+    @variable(m, load_shifted[𝒩ᴸˢ, 𝒯])
 end
